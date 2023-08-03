031ba0ec49e6f7a84c6f0295f61b54d966aecd03
Elvin <eanunez25@gmail.com>
2023-08-03 08:37:54 -0500

    add playRound function

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  let randInt = Math.floor(Math.random() * 3) + 1
+  let randInt = Math.floor(Math.random() * 3) + 1;
   let computerChoice;
 
   if (randInt == 1) {
@@ -14,4 +14,22 @@ function getComputerChoice() {
   return computerChoice
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return `Tie game! Both players chose ${playerSelection}.`
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "Computer wins! Paper beats rock."
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "Player wins! Rock beats scissors."
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "Player wins! Paper beats rock."
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "Computer wins! Scissors beat paper."
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "Computer wins! Rock beats scissors."
+  } else {
+    return "Player wins! Scissors beat paper."
+  }
+}
+
+console.log(playRound("rock", getComputerChoice()));
